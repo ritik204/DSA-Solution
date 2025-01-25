69760579950dd48e37bf40e4b14a5e1c6eac886b
ritik204 <65644906+ritik204@users.noreply.github.com>
2025-01-25 15:13:35 +0530

    Time: 1 ms (94.17%), Space: 45.1 MB (94.22%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int total = 0;
+        for(int i :nums){
+            total+=i;
+        }
+        int leftsum = 0;
+
+        for(int i = 0;i<nums.length;i++){
+            if(leftsum == total - leftsum -nums[i])
+                return i;
+            leftsum+=nums[i];
+        }
+        return -1;
+        
+
+        
+    }
+}
